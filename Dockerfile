665c35091e9240062786ad55dfc25cb7877ecb6c
ColoradoStark <i@repulse.com>
2020-12-19 03:38:52 +0200

    removed subdirectory from all files and updated compose

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM caddy:2.2.1
 
 EXPOSE 80 443
 
-RUN mkdir /var/www/ws \
-    && chown -R root /var/www/ws \
+RUN mkdir /var/www \
+    && chown -R root /var/www \
     && apk add wget 
 
-WORKDIR /var/www/ws
+WORKDIR /var/www
 
 RUN wget https://raw.githubusercontent.com/ColoradoStark/caddy-ssl-multisite/main/Caddyfile \
     && mv Caddyfile /etc/caddy/Caddyfile 
--- caddy-setup.sh
+++ caddy-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker container run --name caddy-multisite-server -d -p 80:80 -p 443:443 -v $(pwd):/var/www/ws coloradostark/caddy-ssl-multisite
+docker container run --name caddy-multisite-server -d -p 80:80 -p 443:443 -v $(pwd):/var/www coloradostark/caddy-ssl-multisite
 
 wget https://github.com/ColoradoStark/caddy-ssl-multisite/raw/main/websites.tar
 
