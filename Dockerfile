f5f142ef247ae2350a01699eb83d27333b2147c4
ColoradoStark <i@repulse.com>
2020-12-20 00:27:40 +0200

    support for Node.js

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,13 @@ RUN mkdir /var/www \
     && chown -R root /var/www \
     && apk add wget 
 
+WORKDIR /var/www
 
 RUN wget https://raw.githubusercontent.com/ColoradoStark/caddy-ssl-multisite/main/Caddyfile \
     && mv Caddyfile /etc/caddy/Caddyfile 
 
 
-WORKDIR /var/www
+
 
 
 
--- node-app/server.js
+++ node-app/server.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
+  if (filename == './') {filename += 'index.htm';}
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
