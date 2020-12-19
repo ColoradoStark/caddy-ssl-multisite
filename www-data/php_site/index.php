b7f5a63d6e417668edfdaeca8b0a90a2c4452f43
ColoradoStark <i@repulse.com>
2020-12-19 14:55:40 +0200

    show php info

--- www-data/php_site/index.php
+++ www-data/php_site/index.php
@@ -1,21 +1,6 @@
-<html>
-<head>
-    <title>PHP Demo</title>
-    <style type="text/css">
-        #wrap {
-            width: 900px;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-<body id="wrap">
-    
-    <?php 
-        // Test if PHP works
-        echo '<h1><p>If you see this message in Bold PHP is working</p></h1>';
-    ?>
+<?php
 
+// Show all information, defaults to INFO_ALL
+phpinfo();
 
-
-</body>
-</html>
+?>
