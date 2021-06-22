6c5e88a7b165d18e4b4619c95e4d49d01899dabd
Colorado Stark <coloradostark@yahoo.com>
2021-06-22 20:11:02 +0300

    added env variables

--- caddy-setup.sh
+++ caddy-setup.sh
@@ -4,6 +4,10 @@ wget https://raw.githubusercontent.com/ColoradoStark/caddy-ssl-multisite/main/do
 
 wget https://github.com/ColoradoStark/caddy-ssl-multisite/raw/master/websites.tar
 
+wget https://raw.githubusercontent.com/ColoradoStark/caddy-ssl-multisite/master/db-variables.env
+
+wget https://raw.githubusercontent.com/ColoradoStark/caddy-ssl-multisite/master/wp-variables.env
+
 tar -xvf websites.tar
 
 rm websites.tar
