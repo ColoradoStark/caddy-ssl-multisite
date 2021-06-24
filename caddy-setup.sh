81c134cbe8881f34e1632a0736a02ec576d35601
Colorado Stark <coloradostark@yahoo.com>
2021-06-24 14:19:57 +0300

    Update caddy-setup.sh

--- caddy-setup.sh
+++ caddy-setup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Install Docker CE
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# Elevate Permissions for Ubuntu user 
+sudo usermod -aG docker ubuntu
+
+# Install Docker Machine
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 mkdir caddy
 
 cd caddy
